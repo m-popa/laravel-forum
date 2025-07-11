e311b06efd0edd6a17a261f127ad8683f05391fd
Mihai Popa <m-popa@users.noreply.github.com>
2025-07-11 19:14:24 +0300

    paginated comments

--- app/Livewire/Thread/ListComments.php
+++ app/Livewire/Thread/ListComments.php
@@ -6,30 +6,41 @@ use App\Models\Thread;
 use Livewire\Component;
 use Illuminate\View\View;
 use Livewire\Attributes\On;
+use Livewire\WithPagination;
 use Livewire\Attributes\Locked;
 use Livewire\Attributes\Computed;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class ListComments extends Component
 {
+    use WithPagination;
+
     #[Locked]
     public int $threadId;
 
-    #[Computed]
-    public function thread(): Thread
-    {
-        return Thread::with('comments.user')->findOrFail($this->threadId);
-    }
+    public int $page = 1;
 
     #[On('replyCreated')]
     public function refreshReplies(): void
     {
+        $lastPage   = $this->comments()->lastPage();
+        $this->page = $lastPage;
         $this->dispatch('$refresh');
+    }
+
+    #[Computed]
+    public function comments(): LengthAwarePaginator
+    {
+        return Thread::findOrFail($this->threadId)
+                     ->comments()
+                     ->with('user')
+                     ->paginate(10, ['*'], 'page', $this->page);
     }
 
     public function render(): View
     {
         return view('livewire.thread.list-comments', [
-            'comments' => $this->thread->comments,
+            'comments' => $this->comments,
         ]);
     }
 }
--- resources/views/livewire/thread/list-comments.blade.php
+++ resources/views/livewire/thread/list-comments.blade.php
@@ -1,8 +1,11 @@
-<div
-    class="space-y-8 bg-white dark:bg-gray-900 mt-8">
+<div class="space-y-8 bg-white dark:bg-gray-900 mt-8">
     @forelse ($comments as $comment)
         <x-thread.comment-card :comment="$comment"/>
     @empty
-        <p class="text-gray-500 dark:text-gray-400">No replies yet. Be the first to comment!</p>
+        <p class="text-gray-500 dark:text-gray-400">
+            No comments yet. Be the first to comment!
+        </p>
     @endforelse
+
+    {{ $comments->links() }}
 </div>
